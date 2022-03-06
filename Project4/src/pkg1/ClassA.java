e4364c34dbb60b903498e7fcd86c1eb881a20869
deepakchanana <deepakdeep1992@gmail.com>
2022-03-06 21:58:55 +0530

    pushing the code first time

---
+++ Project4/src/pkg1/ClassA.java
@@ -0,0 +1,14 @@
+package pkg1;
+
+import org.testng.annotations.Test;
+
+public class ClassA 
+{
+
+	@Test
+	public void method1()
+	{
+		System.out.println("****************ClassA method***************");
+	}
+	
+}
